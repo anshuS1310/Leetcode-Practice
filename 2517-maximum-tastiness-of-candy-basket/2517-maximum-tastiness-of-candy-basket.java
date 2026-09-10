43ac8ea8ecdee295daf33c7bda28403dfd87414a
Anshu <anshu.s1310@gmail.com>
2026-09-11 02:51:23 +0530

    Time: 47 ms (56.67%), Space: 66 MB (44.89%) - LeetHub

---
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+class Solution {
+    private static boolean isp(int [] arr, int k, int m){
+        int j=1;
+        int l=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-l>=m){
+                j++;
+                l=arr[i];
+            }
+            if(j>=k){
+                return true;
+            }
+        }
+        return false;
+    }
+    public int maximumTastiness(int[] arr, int k) {
+        Arrays.sort(arr);
+        int l=0,r=arr[arr.length-1]-arr[0];
+        int res=0;
+        while(l<=r){
+            int md=(l+r)/2;
+            if(isp(arr,k,md)){
+                res=md;
+                l=md+1;
+            }else{
+                r=md-1;
+            }
+        }  
+        return res;
+    }
+}
