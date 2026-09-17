3cbd1b1771039806b6217de84673158136786f94
Anshu <anshu.s1310@gmail.com>
2026-09-17 14:51:23 +0530

    Time: 13 ms (99.59%), Space: 82.1 MB (96.17%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,31 +1,43 @@
 class Solution {
-    private static boolean chk(int [] ar,int m,int k,int d){
-        int bq=0,c=0;
-        for(int i=0;i<ar.length;i++){
-            if(ar[i]<=d){
-                c++;
-            }else{
-                bq+=c/k;
-                c=0;
-            }
-        }
-        bq+=c/k;
-        return bq>=m;
-    }
     public int minDays(int[] bloomDay, int m, int k) {
-        int mn=0,mx=Integer.MIN_VALUE,res=-1;
-        for(int i:bloomDay){
-            mx=Math.max(i,mx);
+        if ((long) m * k > bloomDay.length) {
+            return -1;
         }
-        while(mn<=mx){
-            int md=(mn+mx)/2;
-            if(chk(bloomDay,m,k,md)){
-                res=md;
-                mx=md-1;
-            }else{
-                mn=md+1;
+
+        int low = 1, high = (int) 1e9;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (isPossibleBouquets(bloomDay, m, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        };
+
+        return low;
+    }
+    private boolean isPossibleBouquets(int[] bloomDay, int m, int k, int day) {
+        int total = 0;
+
+        for (int i = 0; i < bloomDay.length; i++) {
+            int count = 0;
+            while (i < bloomDay.length && count < k && bloomDay[i] <= day) {
+                count++;
+                i++;
+            }
+
+            if (count == k) {
+                total++;
+                i--;
+            }
+
+            if (total >= m) {
+                return true;
             }
         }
-        return res;
+
+        return false;
     }
+
 }
