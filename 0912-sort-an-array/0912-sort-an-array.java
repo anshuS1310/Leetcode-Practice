77b7b53ce6baced520c4b0198810e86a5407d659
Anshu <anshu.s1310@gmail.com>
2026-09-20 16:19:04 +0530

    Time: 33 ms (24.61%), Space: 83.1 MB (70.02%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,22 +1,39 @@
 class Solution {
+    static void merge(int [] a,int l,int m ,int r){
+        int n1=m-l+1,n2=r-m;
+        int l1[]=new int[n1];
+        int l2[]=new int[n2];
+        for(int i=0;i<n1;i++){
+            l1[i]=a[l+i];
+        }
+        for(int i=0;i<n2;i++){
+            l2[i]=a[m+1+i];
+        }
+        int i=0,j=0,k=l;
+        while(i<n1 && j<n2){
+            if(l1[i]<=l2[j]){
+                a[k++]=l1[i++];
+            }else{
+                a[k++]=l2[j++];
+            }
+        }
+        while(i<n1){
+            a[k++]=l1[i++];
+        }
+        while(j<n2){
+            a[k++]=l2[j++];
+        }
+    }
+    static void mergest(int [] a, int l ,int r){
+        if(l<r){
+            int m=l+(r-l)/2;
+            mergest(a,l,m);
+            mergest(a,m+1,r);
+            merge(a,l,m,r);
+        }
+    }
     public int[] sortArray(int[] nums) {
-        int mx=Integer.MIN_VALUE,mn=Integer.MAX_VALUE;
-        for(int i:nums){
-            mx=Math.max(i,mx);
-            mn=Math.min(i,mn);
-        }
-        int [] c = new int[mx-mn+1];
-        for(int i=0;i<nums.length;i++){
-            c[nums[i]-mn]++;
-        }
-        for(int i=1;i<=mx-mn;i++){
-            c[i]+=c[i-1];
-        }
-        int [] ans = new int [nums.length];
-        for(int i=0;i<nums.length;i++){
-            ans[c[nums[i]-mn]-1]=nums[i];
-            c[nums[i]-mn]--;
-        }
-        return ans;  
+        mergest(nums,0,nums.length-1);
+        return nums;
     }
 }
