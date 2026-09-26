366f2dc32137d209911e2f836d981fa41b0c5476
Anshu <anshu.s1310@gmail.com>
2026-09-26 14:36:45 +0530

    Time: 7 ms (23.68%), Space: 45 MB (61.63%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,13 +1,13 @@
 class Solution {
-    private boolean myc(String s1, String s2){
-        return (s1+s2).compareTo(s2+s1)>0;
+    private int myc(String s1, String s2){
+        return (s2+s1).compareTo(s1+s2);
     }
     public String largestNumber(int[] nums) {
         ArrayList<String> stres=new ArrayList<>();
         for(int i:nums){
             stres.add(Integer.toString(i));
         }
-        Collections.sort(stres,(a,b)->myc(a,b) ? -1:1);
+        Collections.sort(stres,(a,b)->myc(a,b));
         if(stres.get(0).equals("0")){
             return "0";
         }
